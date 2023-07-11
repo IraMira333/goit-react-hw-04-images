9f498f9f9caec6d781b6e8f6b546bf49114ae0b4
IraMira333 <irinat333@gmail.com>
2023-07-11 16:36:33 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import css from './ImageGallery.module.css';
 import ImageGalleryItem from 'components/ImageGallery/ImageGalleryItem';
