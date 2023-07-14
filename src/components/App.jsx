be9974c5a28260864f8696faa779ea4bdb415920
IraMira333 <irinat333@gmail.com>
2023-07-14 22:10:17 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { searchImages } from '../API/Api';
 
 import { ToastContainer, toast } from 'react-toastify';
@@ -19,109 +19,87 @@ const toastConfig = {
   theme: 'colored',
 };
 
-export class App extends Component {
-  state = {
-    query: '',
-    images: [],
-    page: 1,
-    totalImages: 0,
-    isLoading: false,
-    error: null,
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalImages, setTotalImages] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.getRequest();
+  useEffect(() => {
+    if (!query) return;
+    async function getRequest() {
+      try {
+        setIsLoading(true);
+
+        const { hits, total, totalHits } = await searchImages(query, page);
+
+        if (!totalHits) {
+          toast.warning(`No images found`, toastConfig);
+          return;
+        }
+
+        if (totalHits > 0 && page === 1) {
+          toast.success(`We found ${total} images`, toastConfig);
+        }
+        const shortimages = hits.map(
+          ({ id, tags, largeImageURL, webformatURL }) => {
+            return { id, tags, largeImageURL, webformatURL };
+          }
+        );
+        console.log(shortimages);
+        setImages(prevState => [...prevState, ...shortimages]);
+        setTotalImages(totalHits);
+      } catch (error) {
+        setError(error => error.message);
+        toast.error(error.message, toastConfig);
+      } finally {
+        setIsLoading(false);
+      }
     }
-  }
+    getRequest();
+  }, [error, page, query]);
 
-  searchImagesInput = query => {
-    if (this.state.query === query) {
+  const searchImagesInput = queryInput => {
+    if (query === queryInput) {
       return;
     }
-    this.setState({ query, page: 1, images: [], totalImages: 0 });
+    setQuery(queryInput);
+    setPage(1);
+    setImages([]);
   };
 
-  handleBtnClick = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
+  const loadMore = () => {
+    setPage(prevState => prevState + 1);
   };
 
-  async getRequest() {
-    try {
-      this.setState({ isLoading: true });
+  return (
+    <div>
+      <Searchbar onSubmit={searchImagesInput} />
 
-      const {
-        hits: images,
-        total,
-        totalHits: totalImages,
-      } = await searchImages(this.state.query, this.state.page);
-
-      if (!totalImages) {
-        toast.warning(`No images found`, toastConfig);
-        return;
-      }
-
-      if (totalImages > 0 && this.state.page === 1) {
-        toast.success(`We found ${total} images`, toastConfig);
-      }
-      const shortimages = images.map(
-        ({ id, tags, largeImageURL, webformatURL }) => {
-          return { id, tags, largeImageURL, webformatURL };
-        }
-      );
-      console.log(shortimages);
-      this.setState(prevState => ({
-        images: [...prevState.images, ...shortimages],
-        totalImages,
-      }));
-    } catch (error) {
-      this.setState({ error: error.message });
-      toast.error(error.message, toastConfig);
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  }
-
-  loadMore = () => {
-    this.setState(prevState => {
-      return {
-        page: prevState.page + 1,
-      };
-    });
-  };
-
-  render() {
-    const { images, totalImages, isLoading } = this.state;
-    return (
-      <div>
-        <Searchbar onSubmit={this.searchImagesInput} />
-
-        {isLoading && <Loader />}
-        {images.length > 0 && <ImageGallery images={images} />}
-        {!isLoading && images.length !== totalImages && (
-          <LoadMoreBtn loadMore={this.loadMore} />
-        )}
-        {/* {images.length > 0 &&
+      {isLoading && <Loader />}
+      {images.length > 0 && <ImageGallery images={images} />}
+      {!isLoading && images.length !== totalImages && (
+        <LoadMoreBtn loadMore={loadMore} />
+      )}
+      {/* {images.length > 0 &&
           totalImages / imagesPerPage > 1 &&
           Math.ceil(totalImages / imagesPerPage) !== page && (
             <LoadMoreBtn loadMore={this.loadMore} />
           )} */}
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-        />
-      </div>
-    );
-  }
-}
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </div>
+  );
+};
